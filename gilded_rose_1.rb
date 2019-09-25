999e5f659d52b31e3643e9e01d2ebe0c9b7de001
Jeremy Abernathy <j.e.abernathy@gmail.com>
2019-09-25 18:07:59 -0400

    First solution to Gilded Rose in Ruby.

---
+++ gilded_rose_1.rb
@@ -0,0 +1,50 @@
+# Simple solution to: https://github.com/jimweirich/gilded_rose_kata
+
+class GildedRose
+  attr_reader :name, :days_remaining, :quality
+
+  def initialize(name:, days_remaining:, quality:)
+    @name, @days_remaining, @quality = name, days_remaining, quality
+  end
+
+  def tick
+    case name
+    when 'normal'
+      normal_tick
+    when 'Aged Brie'
+      brie_tick
+    when 'Sulfuras, Hand of Ragnaros'
+      sulfuras_tick
+    when 'Backstage passes to a TAFKAL80ETC concert'
+      backstage_tick
+    end
+  end
+
+  def normal_tick
+    @days_remaining -= 1
+    return if @quality == 0
+    @quality -= 1
+    @quality -= 1 if @days_remaining <= 0
+  end
+
+  def brie_tick
+    @days_remaining -= 1
+    return if @quality >= 50
+    
+    @quality += 1
+    @quality += 1 if @days_remaining <= 0
+  end
+
+  def sulfuras_tick
+  end
+
+  def backstage_tick
+    @days_remaining -= 1
+    return if @quality >= 50
+    return @quality = 0 if @days_remaining < 0
+
+    @quality += 1
+    @quality += 1 if @days_remaining < 10
+    @quality += 1 if @days_remaining < 5
+  end
+end
