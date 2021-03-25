655f19fb7c91ac7f3c1c3901adb37d59052d27a8
Jeremy Abernathy <jea@JEREMYs-MacBook-Pro.local>
2021-03-25 11:06:53 -0400

    Flesh out class w methods.

--- getThreeDiceProbs.js
+++ getThreeDiceProbs.js
@@ -1,15 +1,26 @@
 // todo: return 2d6, 2d8, 3d6 arrays that look like this:
+const input_1 = [8, 6]
+// const input_2 
+// const input_3
 
 class ResultsObj {
-  constructor(rows, sum) {
-    this.rows = rows
-    this.sum = sum
+  constructor(diceArr) {
+    this.diceArr = diceArr
+    this.rows = [0,0]
+    this.sum = 0
+  }
+  calcResults(max, i, arr) {
+    // lower scope
+    // console.log("I am max + 1: ", max+1)
+    // console.log("I am i: ", i)
+    // console.log("I am arr: ", arr)
   }
   getAllResults() {
-
-    console.log("I am a method.")
+    // H.O.O.
+    this.diceArr.map(this.calcResults)
+    // console.log("I am input: ", this.diceArr)
   }
 }
-const matrix2d8 = new ResultsObj([], 0)
+const matrix2d8 = new ResultsObj(input_1)
 
 matrix2d8.getAllResults()
