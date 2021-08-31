4a1fbcb11cf4def2af536e61025fa21b5a50c5c5
Jeremy Abernathy <jea@JEREMYs-MacBook-Pro.local>
2021-08-31 19:31:55 -0400

    Poker deck experiment, for creating RPG Longwinter generator.

---
+++ fiftyTwoTest.js
@@ -0,0 +1,23 @@
+// generate 52 cards that are *shuffled, have a :suit and :face
+// display as rectangles on webpage, georgia font + icons black or red
+
+// ouside in: want to return array of card objects.
+// initialize array with exact spaces, instead of literal?
+const result = new Array(52)
+
+// object literal to hold key:value pairs used to construct deck
+// from least, to most
+// generatorObj = {
+//   // array of objects
+//   suits: [
+//     {
+//       name: 'hearts',
+      
+//     }
+//   ]
+// }
+
+
+
+
+console.log(result)
