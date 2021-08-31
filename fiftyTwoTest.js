c8660994fc7bc34adaf2f10e4a71ad908682f5de
Jeremy Abernathy <jea@JEREMYs-MacBook-Pro.local>
2021-08-31 19:53:59 -0400

    Simple i iterator.

--- fiftyTwoTest.js
+++ fiftyTwoTest.js
@@ -12,7 +12,20 @@ const suits = ['diamonds', 'hearts', 'clubs', 'spades']
 const faces = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'] // generate numeral strings "twos" dynamically as well?
 // logic:
 
+// for (i = 0; i < result.length; i++) {
+//   let newCard = {}
+//   // grab a face
+//   faces.map((currentFace, index, arr) => {
+//     // object literal to hold card values before pushing to output array
+//     newCard["face"] = faces.pop()
 
 
+//   })
+
+  result[i] = i+1
+}
+
+// forEach doesn't execute on empty values?
+// array.forEach(function(currentValue, index, arr), thisValue) 
 
 console.log(result)
